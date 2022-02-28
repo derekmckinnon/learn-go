0433af90d23d4c5f48c5285cea77d79d86b2281f
Derek McKinnon <mckinnon.derek@gmail.com>
2022-02-28 13:35:39 -0500

    Hello, World

---
+++ hello_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Derek")
+		want := "Hello, Derek"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
