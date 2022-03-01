a3007a0f53e14cb06367444018b202ac359a4b89
Derek McKinnon <mckinnon.derek@gmail.com>
2022-03-01 09:41:05 -0500

    Add multi-language support

--- hello_test.go
+++ hello_test.go
@@ -12,16 +12,28 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Derek")
+		got := Hello("Derek", "")
 		want := "Hello, Derek"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Derek", "Spanish")
+		want := "Hola, Derek"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Derek", "French")
+		want := "Bonjour, Derek"
 		assertCorrectMessage(t, got, want)
 	})
 }
