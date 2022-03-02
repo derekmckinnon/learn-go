af20c1427a49f3f3d52210cce2e834175d2caf38
Derek McKinnon <mckinnon.derek@gmail.com>
2022-03-02 10:28:19 -0500

    Arrays and slices: Part 2

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,10 +1,23 @@
 package arrays
 
-func Sum(numbers []int) int {
-	sum := 0
+func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
 
-	return sum
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return
 }
